1f6973d9be253aa7291ceb6e86bb77010fdd7043
Aiden Wolf <2952105383@qq.com>
2022-09-06 08:46:03 +0800

    0906

---
+++ mathcore.py
@@ -0,0 +1,13 @@
+
+
+class Minefield:
+    def __init__(self, field_size = [40, 40]):
+        self.field_size = field_size
+        self.field_list = [[0]*field_size[0]]*field_size[1]
+
+
+    def __random_mine(self, times):
+
+
+if __name__ == '__main__':
+    a = Minefield()
